3ea0f80688e0fa00150f591542263c2474968aa1
“Mories <mories.hutapea@binus.ac.id>
2025-06-26 21:04:23 +0700

    update

--- resources/views/books/edit.blade.php
+++ resources/views/books/edit.blade.php
@@ -6,57 +6,60 @@
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Form Edit Buku</h5>
 
-                        <form action="{{ route('books.update', $book) }}" method="POST" class="row g-3">
-                            @csrf
-                            @method('PUT')
+                <h5 class="card-title">Form Edit Buku</h5>
 
-                            <div class="col-md-6">
-                                <label for="title" class="form-label">Judul Buku</label>
-                                <input type="text" name="title" id="title" value="{{ old('title', $book->title) }}" required class="form-control @error('title') is-invalid @enderror">
-                                @error('title')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
+                <form action="{{ route('books.update', $book) }}" method="POST" class="row g-3">
+                    @csrf
+                    @method('PUT')
 
-                            <div class="col-md-6">
-                                <label for="author" class="form-label">Penulis</label>
-                                <input type="text" name="author" id="author" value="{{ old('author', $book->author) }}" required class="form-control @error('author') is-invalid @enderror">
-                                @error('author')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <div class="col-md-6">
-                                <label for="published_year" class="form-label">Tahun Terbit</label>
-                                <input type="number" name="published_year" id="published_year" value="{{ old('published_year', $book->published_year) }}" class="form-control @error('published_year') is-invalid @enderror">
-                                @error('published_year')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <div class="col-md-12">
-                                <label for="description" class="form-label">Deskripsi</label>
-                                <textarea name="description" id="description" rows="4" class="form-control @error('description') is-invalid @enderror">{{ old('description', $book->description) }}</textarea>
-                                @error('description')
-                                    <div class="invalid-feedback">{{ $message }}</div>
-                                @enderror
-                            </div>
-
-                            <div class="text-center mt-4">
-                                <a href="{{ route('books.index') }}" class="btn btn-secondary">
-                                    <i class="bi bi-arrow-left"></i> Batal
-                                </a>
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="bi bi-save"></i> Perbarui
-                                </button>
-                            </div>
-                        </form>
+                    <div class="col-md-6">
+                        <label for="title" class="form-label">Judul Buku</label>
+                        <input type="text" name="title" id="title" value="{{ old('title', $book->title) }}" required
+                               class="form-control @error('title') is-invalid @enderror">
+                        @error('title')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
                     </div>
-                </div>
+
+                    <div class="col-md-6">
+                        <label for="author" class="form-label">Penulis</label>
+                        <input type="text" name="author" id="author" value="{{ old('author', $book->author) }}" required
+                               class="form-control @error('author') is-invalid @enderror">
+                        @error('author')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-6">
+                        <label for="published_year" class="form-label">Tahun Terbit</label>
+                        <input type="number" name="published_year" id="published_year"
+                               value="{{ old('published_year', $book->published_year) }}"
+                               class="form-control @error('published_year') is-invalid @enderror">
+                        @error('published_year')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="col-md-12">
+                        <label for="description" class="form-label">Deskripsi</label>
+                        <textarea name="description" id="description" rows="4"
+                                  class="form-control @error('description') is-invalid @enderror">{{ old('description', $book->description) }}</textarea>
+                        @error('description')
+                        <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="text-center mt-4">
+                        <a href="{{ route('books.index') }}" class="btn btn-secondary">
+                            <i class="bi bi-arrow-left"></i> Batal
+                        </a>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="bi bi-save"></i> Perbarui
+                        </button>
+                    </div>
+                </form>
+
             </div>
         </div>
     </section>
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -6,91 +6,92 @@
     <section class="section profile">
         <div class="row">
             <div class="col-xl-12">
-                <div class="card">
-                    <div class="card-body pt-3">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h5 class="card-title">Detail Informasi Buku</h5>
-                            <div>
-                                <a href="{{ route('books.edit', $book) }}" class="btn btn-warning btn-sm">
-                                    <i class="bi bi-pencil-square"></i> Edit
-                                </a>
-                                <a href="{{ route('books.index') }}" class="btn btn-secondary btn-sm">
-                                    <i class="bi bi-arrow-left"></i> Kembali
-                                </a>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Informasi Buku</h5>
-                                        <div class="row mb-3">
-                                            <div class="col-lg-4 col-md-4 label">Judul</div>
-                                            <div class="col-lg-8 col-md-8">{{ $book->title }}</div>
-                                        </div>
 
-                                        <div class="row mb-3">
-                                            <div class="col-lg-4 col-md-4 label">Penulis</div>
-                                            <div class="col-lg-8 col-md-8">{{ $book->author }}</div>
-                                        </div>
-
-                                        <div class="row mb-3">
-                                            <div class="col-lg-4 col-md-4 label">Tahun Terbit</div>
-                                            <div class="col-lg-8 col-md-8">{{ $book->published_year ?: 'Tidak tersedia' }}</div>
-                                        </div>
-                                    </div>
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h5 class="card-title">Detail Informasi Buku</h5>
+                    <div>
+                        <a href="{{ route('books.edit', $book) }}" class="btn btn-warning btn-sm">
+                            <i class="bi bi-pencil-square"></i> Edit
+                        </a>
+                        <a href="{{ route('books.index') }}" class="btn btn-secondary btn-sm">
+                            <i class="bi bi-arrow-left"></i> Kembali
+                        </a>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Informasi Buku</h5>
+                                <div class="row mb-3">
+                                    <div class="col-lg-4 col-md-4 label">Judul</div>
+                                    <div class="col-lg-8 col-md-8">{{ $book->title }}</div>
                                 </div>
-                            </div>
 
-                            <div class="col-lg-6">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Deskripsi</h5>
-                                        <div class="card-text">
-                                            {!! nl2br(e($book->description)) ?: '<p class="text-muted fst-italic">Tidak ada deskripsi</p>' !!}
-                                        </div>
-                                    </div>
+                                <div class="row mb-3">
+                                    <div class="col-lg-4 col-md-4 label">Penulis</div>
+                                    <div class="col-lg-8 col-md-8">{{ $book->author }}</div>
+                                </div>
+
+                                <div class="row mb-3">
+                                    <div class="col-lg-4 col-md-4 label">Tahun Terbit</div>
+                                    <div class="col-lg-8 col-md-8">{{ $book->published_year ?: 'Tidak tersedia' }}</div>
                                 </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div class="row mt-4">
-                            <div class="col-lg-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">Informasi Tambahan</h5>
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="row mb-3">
-                                                    <div class="col-lg-4 col-md-4 label">Ditambahkan pada</div>
-                                                    <div class="col-lg-8 col-md-8">{{ $book->created_at->format('d M Y H:i') }}</div>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-6">
-                                                <div class="row mb-3">
-                                                    <div class="col-lg-4 col-md-4 label">Terakhir diperbarui</div>
-                                                    <div class="col-lg-8 col-md-8">{{ $book->updated_at->format('d M Y H:i') }}</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                    <div class="col-lg-6">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Deskripsi</h5>
+                                <div class="card-text">
+                                    {!! nl2br(e($book->description)) ?: '<p class="text-muted fst-italic">Tidak ada deskripsi</p>' !!}
                                 </div>
-                            </div>
-                        </div>
-
-                        <div class="row mt-4">
-                            <div class="col-lg-12 d-flex justify-content-end">
-                                <form action="{{ route('books.destroy', $book) }}" method="POST" onsubmit="return confirm('Apakah Anda yakin ingin menghapus buku ini?')">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">
-                                        <i class="bi bi-trash"></i> Hapus Buku
-                                    </button>
-                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
+
+                <div class="row mt-4">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Informasi Tambahan</h5>
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                        <div class="row mb-3">
+                                            <div class="col-lg-4 col-md-4 label">Ditambahkan pada</div>
+                                            <div
+                                                class="col-lg-8 col-md-8">{{ $book->created_at->format('d M Y H:i') }}</div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <div class="row mb-3">
+                                            <div class="col-lg-4 col-md-4 label">Terakhir diperbarui</div>
+                                            <div
+                                                class="col-lg-8 col-md-8">{{ $book->updated_at->format('d M Y H:i') }}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row mt-4">
+                    <div class="col-lg-12 d-flex justify-content-end">
+                        <form action="{{ route('books.destroy', $book) }}" method="POST"
+                              onsubmit="return confirm('Apakah Anda yakin ingin menghapus buku ini?')">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">
+                                <i class="bi bi-trash"></i> Hapus Buku
+                            </button>
+                        </form>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
