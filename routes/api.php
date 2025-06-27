fa4bdf314483dec6b0ad9780ccfa75bd7c05bc59
“Mories <mories.hutapea@binus.ac.id>
2025-06-27 19:31:18 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\Api\BookApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('books', BookApiController::class);
+Route::apiResource('books', BookApiController::class)->names([
+    'index' => 'api.books.index',
+    'show' => 'api.books.show',
+    'store' => 'api.books.store',
+    'update' => 'api.books.update',
+    'destroy' => 'api.books.destroy',
+]);
+
